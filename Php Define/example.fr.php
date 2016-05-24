b5246b0b1748ca5ed79d0db1934ebff8351f5eba
Markus Merzinger <maerzbow@gmail.com>
2016-05-24 16:03:50 +0200

    Adds Php Define example files

---
+++ Php Define/example.fr.php
@@ -0,0 +1,11 @@
+<?php
+ define("welcome", "Hurrah, tu es là. Ça fait une bonne journée - profite des renseignements suivants !");
+
+ define("text_segment", "Un tel extrait est qualifié de segment du texte chez nous. C’est parti – commence tes traductions tout de suite/maintenant !");
+
+ define("tab_shortcut", "Arriba, Arriba ! Andale, Andale ! Prends Speedy Gonzales de vitesse. Appuie sur TAB pour sauvegarder et continuer avec le segment prochain dès que tu as fini.");
+
+ define("placeholder_lingochecks", "Bazinga !");
+
+ define("love", "❤");
+?>
